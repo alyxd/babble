706499f87c93a0d07e8558752d28dc6739093264
Ella Rose <python_pride@protonmail.com>
2020-09-18 18:27:59 -0400

    removed `load_list` from api

--- api.py
+++ api.py
@@ -1,11 +1,9 @@
-VERSION = "0.0.1-beta.1"
+VERSION = "0.0.1-beta.2"
 LANGUAGE = "python"
 PROJECT = "babble"
 
 API = {"babble.generate_phrase" : {"arguments" : ("length int", ),
                                    "keywords" : {"_list" : "list"},
                                    "returns" : "str",
-                                   "exceptions" : ("ValueError", "IOError")},
-       "babble.load_list" : {"keywords" : {"filename" : "str"},
-                             "returns" : ("list", )}
+                                   "exceptions" : ("ValueError", "IOError")}       
        }
