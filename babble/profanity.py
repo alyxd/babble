683f141c8764c89cdab60750244bcdb56f676f7c
Ella Rose <python_pride@protonmail.com>
2020-09-18 15:27:29 -0400

    profanity filter

---
+++ babble/profanity.py
@@ -0,0 +1,10 @@
+# this contains the words that definitely should not end up in the word list.
+# it necessarily contains profanity, obscenity, and hateful content.
+# the values are base64 encoded so such obsceneity need not be witnessed
+
+FILTER = ('ZnVjaw==', 'ZnVja3M=', 'ZGFtbmVk', 'YmFzdGFyZA==', 'aGVsbA==',
+          'ZGFtbg==', 'ZGljaw==', 'Y29jaw==', 'Y3VudA==', 'cHVzc3k=', 'dGl0',
+          'dGl0cw==', 'dGl0dGllcw==', 'YXNz', 'Y3Vt', 'c2V4dWFs', 'c2V4',
+          'c2V4eQ==', 'cG9ybg==', 'cG9ybm8=', 'cG9ybm9ncmFwaHk=', 'Yml0Y2g=',
+          'ZmFn', 'ZmFnZ290', 'bmlnZ2Vy', 'dHJhbm55', 'a2lrZQ==', 'c3BpYw==',
+          'Z29vaw==', 'cG9vcA==', 'Yml0Y2h5', 'c2hpdA==', 'cGlzcw==')
--- babble/wordlistgeneration.py
+++ babble/wordlistgeneration.py
@@ -1,19 +1,23 @@
 import string
+import base64
 
-def generate_wordlist_from(filename, letters=string.letters):
+import babble.profanity
+
+def generate_wordlist_from(filename, letters=string.letters,
+                           profanity_filter=babble.profanity.FILTER):
     wordlist = []
+    encode = base64.standard_b64encode
     with open(filename, 'r') as _file:
         for line in _file.readlines():
             wordlist.extend(word.strip().lower() for word in line.split(' ') if
                             (not set(word).difference(letters)) and
-                            word)
+                            word and
+                           encode(word.strip().lower()) not in profanity_filter)
     return list(sorted(set(wordlist), key=len)) # set removes duplicates
 
 def main():
     filename = "/home/e/Downloads/bookofshadows/podsbos7.txt"
     wordlist = generate_wordlist_from(filename)
-    print len(wordlist)
-    print wordlist[4000:4096]
     with open("wordlist.py", "w") as _file:
         _file.write("WORDLIST =\\\n")
         _file.write('(')
